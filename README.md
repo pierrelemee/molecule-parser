607ff5904aef7a6640b00ff883cef93d2efeccea
Pierre Lemée <pierre@pierrelemee.fr>
2020-07-02 19:54:49 +0200

    Turn main script as a CLI tool + READM doc update

--- README.md
+++ README.md
@@ -14,3 +14,34 @@ Installation and executions are performed upon `pipenv`:
 pipenv install
 pipenv run python main.py
 ``` 
+
+
+## Usage
+
+The main script `main.py` accepts as many arguments as formulas to be parsed.
+ 
+In addition, you can specify two options:
+- `--stdin` (or `-i`): parse each line from standard input as a unique formula
+- `--json` (or `-json`): encode all the flattened formulas into an unique JSON payload
+
+Examples:
+
+```bash
+$ cat formulas.txt | pipenv run python main.py 'O((CN2)3K)4'
+{'O': 1, 'K': 4, 'C': 12, 'N': 24}
+# Parse formulas from formulas.txt file and encode as JSON 
+$ cat formulas.txt | pipenv run python main.py --stdin -j
+[
+    {
+        "C": 12,
+        "K": 4,
+        "N": 24,
+        "O": 1
+    },
+    {
+        "H": 2,
+        "Mg": 1,
+        "O": 2
+    }
+]
+```
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import json
 from molecule import MoleculeParser
+import sys
 
 if __name__ == '__main__':
-    water = 'H2O'
-    print(MoleculeParser.parse(water).get_atoms())
-    magnesium_hydroxide = "Mg(OH)2"
-    print(MoleculeParser.parse(magnesium_hydroxide).get_atoms())
-    fremy_salt = 'K4[ON(SO3)2]2'
-    print(MoleculeParser.parse(fremy_salt).get_atoms())
-    propanal = "CH3CH2CHO"
-    print(MoleculeParser.parse(propanal).get_atoms())
-    dihydroxyacetone = "CO(CH2OH)2"
-    print(MoleculeParser.parse(dihydroxyacetone).get_atoms())
+    # Whether to read data from stdin or
+    read_from_stdin = False
+    write_as_json = False
+    formulas = []
+    for arg in sys.argv[1:]:
+        if arg.lower() == '--stdin' or arg.lower() == '-i':
+            read_from_stdin = True
+        elif arg.lower() == '--json' or arg.lower() == '-j':
+            write_as_json = True
+        else:
+            formulas.append(arg)
+
+
+
+    if read_from_stdin:
+        formulas = list(map(lambda line: line.rstrip(), sys.stdin.readlines()))
+
+    if write_as_json:
+        print(json.dumps(list(map(lambda formula: MoleculeParser.parse(formula).get_atoms(), formulas)), sort_keys=True, indent=4))
+    else:
+        for formula in formulas:
+            print(MoleculeParser.parse(formula).get_atoms())
